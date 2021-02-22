4fad1080074e2b500d22f9326c851886db4d079c
Steve <torvista@users.noreply.github.com>
2021-02-22 23:47:09 +0100

    short array syntax

--- includes/auto_loaders/config.emailFor404.php
+++ includes/auto_loaders/config.emailFor404.php
@@ -2,12 +2,12 @@
 /**
  * Autoloader to instantiate observer class
  */
-$autoLoadConfig[200][] = array(
+$autoLoadConfig[200][] = [
     'autoType' => 'class',
     'loadFile' => 'observers/class.emailFor404.php'
-);
-$autoLoadConfig[200][] = array(
+];
+$autoLoadConfig[200][] = [
     'autoType' => 'classInstantiate',
     'className' => 'emailFor404',
     'objectName' => 'emailFor404'
-);
+];
